84703a23aae266af7ccf2811068c9e57e86cd713
Brett Gossage <bgossagen@gmail.com>
2017-04-06 18:53:50 -0500

    Self-contained and implements both Part I and PartII
    Converting from a FASTA DNA sequence to music (Part I) is done when a
    .fasta file is provided.  Part II, converting from music to a DNA
    sequence FASTA file, is done when a .smf file is input.  Both kinds of
    files are submitted as a command-line argument.

--- src/Group_Project.py
+++ src/Group_Project.py
@@ -6,106 +6,97 @@ Created on Mon Mar 27 20:21:44 2017
 @authors: BYS602 Spring 2017ll
 """
 
+# Python library imports.
 import sys
+import os
+import string
+import subprocess
 
+# BioPython module imports.
 from Bio import SeqIO
-
-import lilypond
-
-input_filename = "../data/human_ins.fasta"
-
-# If provided, use the filename from the command line arg...
-if( len(sys.argv) > 1 ):
-   input_filename = sys.argv[1] 
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 
 
-###############################################################################
-
-
-infile = SeqIO.read( input_filename, "fasta" )
-OutFile = "outfile.txt"
-#Deactivate the above line and activate the below line if you wish to output the path of the fasta file in the terminal
-#OutFile = input(">")
-outfile = open(OutFile,"w")
-
-outfile.write("***********************************************************\n\n")
-outfile.write("This project contains 2 parts.\n")
-outfile.write("Part I is to convert a DNA sequence into a musical note and play it.\n")
-outfile.write("Part II is to convert a new musical note into DNA and do a sequence alignment in BLAST/NCBI.\n\n")
-outfile.write("***********************************************************\n")
+#***********************************************************
+#This project contains 2 parts.
+#Part I is to convert a DNA sequence into a musical note and play it.\n")
+#Part II is to convert a new musical note into DNA and do a sequence alignment in BLAST/NCBI.\n\n")
+#***********************************************************\n")
 #PART 1 of project follows
-outfile.write("PART I: \n")
-outfile.write("***********************************************************\n\n")
-outfile.write("Fasta file has the following information:\n\n")
-outfile.write(str(infile))
-outfile.write("\n\n***********************************************************\n")
+
 
 ###############################################################################
 
-# rules for abbreviations, C0 is low C, C1 is middle C, C2 is high C, C3 is highest C, etc.
-# rules for abbreviations, e = eighth note, q = quarter note
-    # these are determined by the last of the nucleotide triplet, or the wobble position
-        # if U or A it will be an eighth note
-        # if C or G it will be a quarter note
+"""
+Define a dictionary to map codons to notes..
 
-# below part before the colon is from codon table: after the colon is the musical note
+ Rules for abbreviations: C0 is low C, C1 is middle C, C2 is high C, C3 is highest C, etc.
+                          e = eighth note, q = quarter note
+    These are determined by the last of the nucleotide triplet, or the wobble position
+         if U or A it will be an eighth note
+         if C or G it will be a quarter note
 
-NoteDictionary = {"UUU":"C0e", "UUC":"C0q", "UUA":"G0e", 
-                  "UUG":"G0q", "CUU":"C1e", "CUC":"C1q", 
-                  "CUA":"C2e", "CUG":"C2q", "AUU":"A1e", 
+  For the key/value pairs: the part before the colon is from codon table,
+                           after the colon is the musical note or "stop"
+"""
+NoteDictionary = {"UUU":"C0e", "UUC":"C0q", "UUA":"G0e",
+                  "UUG":"G0q", "CUU":"C1e", "CUC":"C1q",
+                  "CUA":"C2e", "CUG":"C2q", "AUU":"A1e",
                   "AUC":"A1q", "AUA":"B0e", "AUG":"B0q",
-                  "GUU":"A2e", "GUC":"A2q", "GUA":"B2e", 
-                  "GUG":"B2q", "UCU":"D0e", "UCC":"D0q", 
-                  "UCA":"A0e", "UCG":"A0q", "CCU":"D1e", 
-                  "CCC":"D1q", "CCA":"D2e", "CCG":"D2q", 
+                  "GUU":"A2e", "GUC":"A2q", "GUA":"B2e",
+                  "GUG":"B2q", "UCU":"D0e", "UCC":"D0q",
+                  "UCA":"A0e", "UCG":"A0q", "CCU":"D1e",
+                  "CCC":"D1q", "CCA":"D2e", "CCG":"D2q",
                   "ACU":"G3e", "ACC":"G3q", "ACA":"A3e",
-                  "ACG":"A3q", "GCU":"B1e", "GCC":"B1q", 
-                  "GCA":"F3e", "GCG":"F3q", "UAU":"E0e", 
-                  "UAC":"E0q", "UAA":"stop", "UAG":"stop", 
-                  "CAU":"E1e", "CAC":"E1q", "CAA":"E2e", 
-                  "CAG":"E2q", "AAU":"B3e", "AAC":"B3q", 
-                  "AAA":"C4e", "AAG":"C4q", "GAU":"D3e", 
-                  "GAC":"D3q", "GAA":"E3e", "GAG":"E3q", 
-                  "UGU":"F0e", "UGC":"F0q", "UGA":"stop", 
-                  "UGG":"C1h", "CGU":"F1e", "CGC":"F1q", 
+                  "ACG":"A3q", "GCU":"B1e", "GCC":"B1q",
+                  "GCA":"F3e", "GCG":"F3q", "UAU":"E0e",
+                  "UAC":"E0q", "UAA":"stop", "UAG":"stop",
+                  "CAU":"E1e", "CAC":"E1q", "CAA":"E2e",
+                  "CAG":"E2q", "AAU":"B3e", "AAC":"B3q",
+                  "AAA":"C4e", "AAG":"C4q", "GAU":"D3e",
+                  "GAC":"D3q", "GAA":"E3e", "GAG":"E3q",
+                  "UGU":"F0e", "UGC":"F0q", "UGA":"stop",
+                  "UGG":"C1h", "CGU":"F1e", "CGC":"F1q",
                   "CGA":"G1e", "CGG":"G1q", "AGU":"F2e",
-                  "AGC":"F2q", "AGA":"G2e", "AGG":"G2q", 
-                  "GGU":"C3e", "GGC":"C3q", "GGA":"A4q", 
+                  "AGC":"F2q", "AGA":"G2e", "AGG":"G2q",
+                  "GGU":"C3e", "GGC":"C3q", "GGA":"A4q",
                   "GGG":"B4q"
                  }
 ###############################################################################
 
-outfile.write("\nThe NoteDictionary used is \n\n")
-outfile.write(str(NoteDictionary))
-ndl = len(NoteDictionary)
-outfile.write("\n\nThe length of the NoteDictionary is ")
-outfile.write(str(ndl)) 
-outfile.write("\n\n***********************************************************\n")
+##
+## Define a function to convert a DNA sequence to musical notes.
+##
+def dna_to_notes( sequence ):
 
-n = 3
-m = [str(infile.seq[i:i+n]) for i in range(0, len(infile.seq)- (len(infile.seq)%3), n)] #Splits into codons.
-outfile.write("\nThe codons are \n\n")
-outfile.write(str(m))
-outfile.write("\n\nThe number of codons available in the input sequence is ")
-outfile.write(str(len(m)))
-outfile.write("\n\n***********************************************************\n\n")
+# Split into codons.
+    codon_length = 3
+    seq_length = len(seq_record.seq)
+
+    # Account for sequence length when not a multiple of the codon_length.
+    seq_range = seq_length-(seq_length%codon_length)
+
+    codons = [str(seq_record.seq[i:i+codon_length]) for i in range(0, seq_range, codon_length)]
+
+    notes = ""
+
+ ## Conversion of Codons to Notes
+ ## The codons converted to musical notes are as follows:
+
+    for codon in codons:
+        codon = codon.replace('T', 'U')
+        note = NoteDictionary[codon]
+        notes += note + ' '
+
+    return notes
+
+#end function dna_to_notes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ###############################################################################
 
-#Conversion of Codons to Notes
-outfile.write("The codons converted to musical notes are as follows: \n\n")
-for codon in m:
-    codon = codon.replace('T', 'U')
-    outfile.write(NoteDictionary[codon] + ' ')
-    
-
-outfile.write("\n\n***********************************************************\n")
-
-###############################################################################            
-
 #PART 2 of project follows
-outfile.write("PART II: \n")
-outfile.write("***********************************************************\n\n")
+
 
 #Back coding Dictionary - from notes to DNA
 CodonDictionary = {'G3q': 'ACC', 'A4q': 'GGA', 'C2q': 'CUG',
@@ -130,38 +121,183 @@ CodonDictionary = {'G3q': 'ACC', 'A4q': 'GGA', 'C2q': 'CUG',
                    'C4e': 'AAA', 'B3q': 'AAC', 'A1e': 'AUU',
                    'A3e': 'ACA', 'B3e': 'AAU'}
 
-MusicalNote = 'C1q G0q E1q D1q A0q F1q B0q D1q F1q A1q G1e G1q F1e E1e C2q G1q E1q D1e F1e A1e D2e F2q G2e A2e G0e E1e G1e C2e B1e D2e F2e B1e B1q C2e D2e C2q'
-outfile.write("The Musical Note given for part 2 of the project is \n\n")
-outfile.write(MusicalNote)
-outfile.write("\n\n")
-MusicalNote = MusicalNote.replace(" ","")
-
 ###############################################################################
 
-#Definition to back code
-def translate(MusicalNote):
+##
+## Define a function to translate musical notes in SMF format to an RNA sequence.
+##
+def translate( musical_notes ):
+
     RNA = ""
-    for i in range(0,len(MusicalNote),3):
-        if MusicalNote[i:i+3] != 'sto' and MusicalNote[i:i+3] != 'pst' and MusicalNote[i:i+3] != 'ops' and MusicalNote[i:i+3] != 'top':
-            codon = MusicalNote[i:i+3]
-            RNA += CodonDictionary[codon] + " "
-        else:
-            for j in range(i,len(MusicalNote),4):
-                if MusicalNote[j:j+4] == 'stop':
-                    codon = MusicalNote[j:j+4]
-                    RNA += CodonDictionary[codon] + " "
+    notes = string.split( musical_notes )
+    for note in notes:
+
+            RNA += CodonDictionary[note]
+
     return RNA
 
-RNA = translate(MusicalNote)
-DNA = RNA.replace("U","T").replace(' ','')
-outfile.write("The DNA sequence encoded by the musical note is \n\n")
-outfile.write(DNA)
-outfile.write("\n\n***********************************************************\n")
+# end function translate ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-###############################################################################
+##
+## Define a functions to transform a Simple Music Format (SMF) string to
+## a lilypond sheet music file.
+##
+def transform_to_lilypond( smf_string, filename="sheet_music.ly" ):
 
-outfile.close()
+    notes = string.split( smf_string )
 
-lilypond.transform_to_lilypond( MusicalNote )
+    with open( filename, "w" ) as output:
+
+    # Write the lilypond header...
+        output.write( "\\version \"2.18.2\"\n" )
+        output.write( "\\score {\n" )
+        output.write( "{\n" )
+
+        lp_note = ""
+
+        for note in notes:
+           try:
+           # Skip stops...
+               if note == 'stop': continue
+
+           # Pitch...
+               lp_note = string.lower( note[0] )
+           # Octave...
+               level = int(note[1])
+               lp_note += "\'" * level
+           # Duration
+               if note[2] == "e": lp_note += "8" # 8 for a eigth note
+               if note[2] == "q": lp_note += "4" # 4 for a quarter note
+               if note[2] == "h": lp_note += "2" # 2 for a half note
+               if note[2] == "w": lp_note += "1" # 1 for a whole note
+
+           # Write the note in lilypond notation...
+               output.write( "{:s} ".format( lp_note ) )
+
+           # Clear the note string for the next note...
+               lp_note = ""
+
+           except(ValueError) as error:
+              msg = str(error) + " for note: " + note
+              raise Exception(msg)
+
+    # Write the lilypond footer...
+        output.write( "\n}\n" )
+        output.write( "\n\\midi { }\n" )
+        output.write( "\n\\layout { }\n" )
+
+        output.write( "\n}\n" )
+
+#end transform_to_lilypond ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+################################################################################
+############################### MAIN PROGRAM ###################################
+################################################################################
+
+# Get the input filename from the command line...
+if( len(sys.argv) < 2 ):
+    print "Usage: " + sys.argv[0] + "  <fasta file>|<smf file>"
+    sys.exit( "Missing Argument" )
+
+input_filename = sys.argv[1]
+
+# Build the output filenames from the input one...
+inputfile_root, inputfile_ext = os.path.splitext( input_filename )
+
+smf_filename = ""
+musical_notes = ""
+valid_input = False
+
+basename = os.path.basename( inputfile_root )
+lilypond_filename = basename + ".ly"
 
 
+# If the input file extension is "fasta", do Part I...
+if inputfile_ext == ".fasta":
+
+    valid_input = True
+
+    print "Converting FASTA sequence to music notes..."
+
+   # Read the input FASTA file using BioPython...
+    seq_record = SeqIO.read( input_filename, "fasta" )
+
+ # Transform to the DNA to notes...
+    musical_notes = dna_to_notes( seq_record.seq )
+
+ # Write the notes an SMF file...
+    smf_filename = basename + ".smf"
+
+    print "Writing SMF file: ", smf_filename
+    with open( smf_filename, "w" ) as output:
+      output.write( musical_notes )
+
+ # We are done with Part I
+
+
+#end if fasta file ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# If given an input notes file, do Part II...
+elif inputfile_ext == ".smf" :
+
+    valid_input = True
+
+    smf_filename = input_filename
+
+    print "Converting Notes to FASTA sequence..."
+
+    with open( input_filename, "r" ) as input_file:
+       musical_notes = input_file.read()
+   #end with
+
+    RNA = translate( musical_notes )
+    DNA = RNA.replace("U","T")
+
+ # Build a BioPython sequence record for output...
+    seq_rec = SeqRecord( seq=Seq(DNA),
+                         id = basename,
+                         description = "musical genome" )
+    rec_list = [ seq_rec ]
+
+ # Build the output filename from the input one...
+    root, ext = os.path.splitext( input_filename )
+    fasta_filename = basename + "_inv.fasta"
+
+ # Write the DNA sequence to a fasta file...
+    print "Writing fasta file: ", fasta_filename
+    SeqIO.write( rec_list, fasta_filename, "fasta" )
+
+#end if smf file ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if valid_input:
+
+ # Write the notes to a lilypond file...
+    lilypond_filename = basename + ".ly"
+    transform_to_lilypond( musical_notes, lilypond_filename )
+
+    """
+    Set generate_music = True if you have installed the required music generation tools:
+      > lilypond (www.lilypond.org) NOTE: configure the path to lilypond accordinly
+      > timidity (linux package for .wav sound file from midi file)
+      > lame (linux package for mp3 generation) 
+    """
+
+    generate_music = True
+    if generate_music:
+        midi_filename = basename + ".midi"
+        mp3_filename = basename + ".mp3"
+
+        lp_cmd = "$HOME/lilypond/usr/bin/lilypond " + lilypond_filename
+        subprocess.check_call( lp_cmd, shell=True )
+        tm_cmd = "timidity -Ow -o output.wav " + midi_filename
+        lm_cmd = "lame -h output.wav " + mp3_filename
+
+        subprocess.check_call( tm_cmd, shell=True )
+        subprocess.check_call( lm_cmd, shell=True )
+
+else:
+# If we get here, we cannot process the input.
+   sys.exit( "Unknown input file type" )
+
+
+# EOF
--- src/lilypond.py
+++ src/lilypond.py
@@ -5,53 +5,60 @@
 """
 
 import string
-import re
 
-MusicalNotes = 'C1h G0q E1q D1q A0q F1q B0q D1q F1q A1q G1e G1q F1e E1e C2h G1q E1q D1e F1e A1e D2e \
-                F2q G2e A2e G0e E1e G1e C2e B1e D2e F2e B1e B1q C2e D2e C2q'
-
-                
-# rules for abbreviations, C0 is low C, C1 is middle C, C2 is high C, C3 is highest C, etc.
-# rules for abbreviations, e = eighth note, q = quarter note
-    # these are determined by the last of the nucleotide triplet, or the wobble position
-        # if U or A it will be an eighth note
-        # if C or G it will be a quarter note
-
+##
+## Transform a Simple Music Format (SMF) string to lilypond sheet music file.
+##
 def transform_to_lilypond( smf_string, filename="sheet_music.ly" ):
-    
-    notes = string.split( smf_string ) 
-    
+
+    notes = string.split( smf_string )
+
     with open( filename, "w" ) as output:
-
+        
+    # Write the lilypond header...
         output.write( "\\version \"2.18.2\"\n" )
-
         output.write( "\\score {\n" )
         output.write( "{\n" )
 
         lp_note = ""
 
         for note in notes:
-            
-           lp_note = string.lower( note[0] )
-           
-           level = int(note[1])
-           lp_note += "\'" * level
-           
-           
-           if note[2] == "e": lp_note += "8"
-           if note[2] == "q": lp_note += "4"
-           if note[2] == "h": lp_note += "2"
-           
-           output.write( "{:s} ".format( lp_note ) )
-           
-           lp_note = ""
-           
+           try:
+           # Skip stops...
+               if note == 'stop': continue
+
+           # Pitch...
+               lp_note = string.lower( note[0] )
+           # Octave...
+               level = int(note[1])
+               lp_note += "\'" * level
+           # Duration
+               if note[2] == "e": lp_note += "8"
+               if note[2] == "q": lp_note += "4"
+               if note[2] == "h": lp_note += "2"
+
+           # Write the note in lilypond notation...
+               output.write( "{:s} ".format( lp_note ) )
+
+           # Clear the note string for the next note...
+               lp_note = ""
+
+           except(ValueError) as error:
+              msg = str(error) + " for note: " + note
+              raise Exception(msg)
+              
+    # Write the lilypond footer...
         output.write( "\n}\n" )
         output.write( "\n\\midi { }\n" )
+        output.write( "\n\\layout { }\n" )
+ 
         output.write( "\n}\n" )
-        
+
 #end transform_to_lilypond
-        
-transform_to_lilypond( MusicalNotes )
+
+#transform_to_lilypond( MusicalNotes )
+
+
+
 
 ## EOF
